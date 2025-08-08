f623409f3685933dd4937567e5be8865c77de794
“sarthakshivgonda” <sarthak.shivgonda@accolitedigital.com>
2025-08-08 20:07:52 +0530

    Added user, address,roles and jwt authentication code

---
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/jwt/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.ecommerce.sb_ecom.security.jwt;
+
+public class LoginRequest {
+
+    private String username;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String password;
+}
